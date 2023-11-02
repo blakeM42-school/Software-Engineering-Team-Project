b003dc61577d9120eb0909ef99b647b9da99d99c
Alevarias <alexjusticesierra@gmail.com>
2023-11-02 14:33:52 -0600

    added script link for footer

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+<script>
+    function displayTime(){
+        function updateTime(){
+            const now = new Date();
+
+            const options = {timeZone: "America/Denver"};
+            const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12:true };
+            const formattedTime = now.toLocaleString('en-US', timeOptions);
+            document.getElementById("time-display").textContent = formattedTime + ", MDT"
+        }
+        updateTime();
+        setInterval(updateTime, 1000);
+    }
+    displayTime();
+</script>
